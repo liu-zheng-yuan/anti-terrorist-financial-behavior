a8613c3c3c9347522d5cc987cc3adbb1f83a0768
fanleehao <fanleehao@gmail.com>
2019-05-08 15:25:09 +0800

    update loadmenu/3

--- src/main/java/edu/nju/antiTerroristFinancialBehavior/service/CommonIndexService.java
+++ src/main/java/edu/nju/antiTerroristFinancialBehavior/service/CommonIndexService.java
@@ -30,8 +30,8 @@ public class CommonIndexService {
 
 
     /**
-     * 查找一二级指标，封装成json串；
-     * 菜单列中只含一二级指标，三级指标将根据二级指标的超链接请求查找（需要另外拼接）
+     * 查找非最细粒度级指标，封装成json串；
+     * 菜单列中只含一二三级指标，四级指标将根据三级指标的超链接请求查找（需要另外拼接）
      * @param id
      * @return
      */
@@ -40,6 +40,7 @@ public class CommonIndexService {
         List<MenuItem> menuItemList = new ArrayList<>();
         int menuID = 1;
         List<FirstIndex> firstIndices = firstIndexMapper.findAllFirstIndex();
+
         if(id == 1){
             //添加一级指标
             for (FirstIndex firstIndex : firstIndices) {
@@ -123,6 +124,48 @@ public class CommonIndexService {
                     for (ThirdIndex thirdIndex : thirdIndices) {
                         thirdIndex.setMenuID(++menuID);
                         MenuItem thirdMenuItem = new MenuItem("/everyIndexMark?indexId=" + thirdIndex.getId(), false,
+                                0, secondIndex.getMenuID(), "", "", 4,
+                                thirdIndex.getMenuID(), thirdIndex.getIndex_name(), 0,"");
+                        menuItemList.add(thirdMenuItem);
+                    }
+                }
+            }
+        }
+
+        /**
+         * 加载权重展示的菜单
+         */
+        else if(id == 3){
+            //添加一级指标
+            for (FirstIndex firstIndex : firstIndices) {
+                menuID++;
+                firstIndex.setMenuID(menuID);
+                MenuItem firstMenuItem = new MenuItem("/weightShow?indexId=" + firstIndex.getId(), false, 0, 0, "",
+                        "", 2, firstIndex.getMenuID(), firstIndex.getIndex_name(), 0,"");
+                menuItemList.add(firstMenuItem);
+            }
+            //添加二级及三级指标
+            for (FirstIndex firstIndex : firstIndices) {
+
+                List<SecondIndex> secondIndices = firstIndexMapper.findSecondIndices(firstIndex.getId());
+
+                for (SecondIndex secondIndex: secondIndices) {
+                    menuID++;
+                    secondIndex.setMenuID(menuID);
+                    MenuItem secondMenuItem = new MenuItem("/weightShow?indexId=" + secondIndex.getId(), false,
+                            0, firstIndex.getMenuID(), "", "", 3,
+                            secondIndex.getMenuID(), secondIndex.getIndex_name(), 0,"");
+                    menuItemList.add(secondMenuItem);
+                }
+
+                /**
+                 * 再依次查找所属三级指标
+                 */
+                for (SecondIndex secondIndex : secondIndices) {
+                    List<ThirdIndex> thirdIndices = secondIndexMapper.findThirdIndicesBySecondIndexId(secondIndex.getId());
+                    for (ThirdIndex thirdIndex : thirdIndices) {
+                        thirdIndex.setMenuID(++menuID);
+                        MenuItem thirdMenuItem = new MenuItem("/weightShow?indexId=" + thirdIndex.getId(), false,
                                 0, secondIndex.getMenuID(), "", "", 4,
                                 thirdIndex.getMenuID(), thirdIndex.getIndex_name(), 0,"");
                         menuItemList.add(thirdMenuItem);
